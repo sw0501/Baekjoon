f63fa2a9e737c310113449206679d5d5d466cbfb
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2022-07-17 12:06:56 +0900

    [Silver II] Title: 별 찍기 - 22, Time: 8 ms, Memory: 4216 KB -BaekjoonHub

---
+++ 백준/Silver/10997. 별 찍기 － 22/별 찍기 － 22.cc
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void init();
+void star(int x,int y,int n,int d);
+
+int N;
+char arr[1500][1500];
+
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,-1,0,1};
+
+int main(){
+	
+	/*
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	*/
+	
+	init();
+	
+	
+	return 0;
+}
+
+void init(){
+	cin >> N;
+	fill(arr[0],arr[1500],' ');
+	
+	if(N==1)cout << "*";
+	
+	else{
+		for(int i=0;i<4*(N-1)+3;i++){
+			arr[i][0] = '*';
+		}
+		for(int i=0;i<4*(N-1)+1;i++){
+			arr[0][i] = '*';
+		}
+		
+		star(0,4*(N-1)+2,4*(N-1)+1,0);
+		for(int i=0;i<=4*(N-1)+2;i++){
+			if(i==1){
+				cout << "*\n";
+			}
+			else{
+				for(int j=0;j<=4*(N-1);j++){
+					cout << arr[i][j];
+				}
+				cout << "\n";
+			}
+			
+		}
+		
+	}
+	
+	//75 119 1513
+	//xy : 4(N-1)+1
+	
+	//5 7 5 5 3 3
+	//9 11 9 9 7 7 5 5 3 3
+	
+}
+
+void star(int x,int y,int n,int d){
+	
+	if(n==1)return;
+	
+	int i = 0;
+	while(i<n-1){
+		y += dy[d];
+		x += dx[d];
+		arr[y][x] = '*';
+		i++;
+	}
+	d++;
+	if(d==4)d=0;
+	i = 0;
+	while(i<n-1){
+		y += dy[d];
+		x += dx[d];
+		arr[y][x] = '*';
+		i++;
+	}
+	d++;
+	if(d==4)d=0;
+	star(x,y,n-2,d);
+}
+
