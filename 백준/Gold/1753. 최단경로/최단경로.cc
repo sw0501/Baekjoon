2b68df1a910b8642f543be97b679fb1ebaf95cfb
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2022-07-18 02:06:09 +0900

    [Gold IV] Title: 최단경로, Time: 96 ms, Memory: 9204 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+int N, M;
+int A, B;
+int INF = 987654321;
+
+vector<pair<int, int>>city[20010];
+int d[20010];			//거리
+bool v[20010];			//도착 확인
+
+void dijkstra(int start) {
+	d[start] = 0;
+	priority_queue<pair<int, int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;	//힙 구조
+	pq.push(make_pair(0, start));
+	while (!pq.empty()) {
+		int current = pq.top().second;
+		int distance = pq.top().first;
+		pq.pop();
+
+		//최단거리가 아니면 스킵
+		if (d[current] >= distance) {
+			for (int i = 0; i < city[current].size(); i++) {
+				//선택된 노드의 인접 노드
+				int next = city[current][i].first;
+				//선택된 노드를 인접 노드를 거쳐서 가는 비용
+				int nextDistance = distance + city[current][i].second;
+				//기존의 최소 비용보다 더 저렴하다면 교체
+				if (nextDistance < d[next]) {
+					d[next] = nextDistance;
+					pq.push(make_pair(nextDistance, next));
+				}
+			}
+		}
+	}
+}
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+
+	int start;
+
+	cin >> start;
+
+	for (int i = 0; i <= N; i++) {
+		d[i] = INF;
+	}
+
+	for (int i = 0; i < M; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		city[a].push_back({ b,c });
+	}
+
+	dijkstra(start);
+
+	for (int i = 1; i <= N; i++) {
+		if (d[i] == INF)cout << "INF" << "\n";
+		else cout << d[i] << "\n";
+	}
+
+	return 0;
+}
