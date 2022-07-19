88d699b6ed68c5dbef1781c077aa8df3a7820dee
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2022-07-19 20:17:22 +0900

    [Gold I] Title: 선분 그룹, Time: 196 ms, Memory: 2376 KB -BaekjoonHub

---
+++ 백준/Gold/2162. 선분 그룹/선분 그룹.cc
@@ -0,0 +1,154 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+struct Point {
+	long long int x, y;
+};
+
+struct Line {
+	int i;
+	Point p1, p2;
+};
+
+
+
+bool operator >= (Point a,Point b) {
+	if (a.x == b.x) {
+		return a.y >= b.y;
+	}
+	return a.x >= b.x;
+}
+
+int N;
+int set[3001] = { 0, };
+int SIZE[3001] = { 0, };
+int MAX = 1;
+int cnt = 1;
+
+vector<Line>v;
+
+int CCW(Point A,Point B, Point C) {
+	int tmp = A.x * B.y + B.x * C.y + C.x * A.y;
+	tmp -= B.x * A.y + C.x * B.y + A.x * C.y;
+	
+	if (tmp > 0)return 1;
+	else if (tmp == 0)return 0;
+	else return -1;
+}
+
+void swap(Point &a, Point &b) {
+	int tx = a.x;
+	int ty = a.y;
+	a.x = b.x;
+	a.y = b.y;
+	b.x = tx;
+	b.y = ty;
+}
+
+int CK(Line l1, Line l2) {
+
+	
+
+	int ans1 = CCW(l1.p1, l1.p2, l2.p1)* CCW(l1.p1, l1.p2, l2.p2);
+	int ans2 = CCW(l2.p1, l2.p2, l1.p1) * CCW(l2.p1, l2.p2, l1.p2);
+	
+
+	if (ans1 == 0 && ans2 == 0) {
+
+		if (l1.p1 >= l1.p2) {
+			swap(l1.p1, l1.p2);
+		}
+		if (l2.p1 >= l2.p2) {
+			swap(l2.p1, l2.p2);
+		}
+
+		if (l2.p2 >= l1.p1 && l1.p2 >= l2.p1)return 1;
+		return 0;
+	}
+	else{
+		if (ans1 <= 0 && ans2 <= 0)return 1;
+		else return 0;
+	}
+}
+
+int getParent(int x) {
+	if (x == set[x])return x;
+	else return set[x] = getParent(set[x]);
+}
+
+void unionParent(Line a, Line b) {
+	int i1, i2;
+	i1 = getParent(a.i);
+	i2 = getParent(b.i);
+	if (i1 == i2) {
+		return;
+	}
+	else if (i1 < i2) {
+		cnt--;
+		set[i2] = i1;
+		SIZE[i1] += SIZE[i2];
+		if (SIZE[i1] > MAX) {
+			MAX = SIZE[i1];
+		}
+	}
+	else {
+		cnt--;
+		set[i1] = i2;
+		SIZE[i2] += SIZE[i1];
+		if (SIZE[i2] > MAX) {
+			MAX = SIZE[i2];
+		}
+	}
+}
+
+int find(Line a, Line b) {
+	a.i = getParent(a.i);
+	b.i = getParent(b.i);
+	if (a.i == b.i)return 1;
+	else return 0;
+}
+
+
+
+int main() {
+
+	cin >> N;
+
+	cnt = N;
+
+	for (int i = 0; i < N; i++) {
+		set[i] = i;
+		SIZE[i] = 1;
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		v.push_back(Line{ i,{x1,y1} ,{x2, y2} });
+	}
+
+	for (int i = 0; i < v.size(); i++) {
+		for (int j = 0; j < v.size(); j++) {
+			if (i == j)continue;
+			//if (find(v[i], v[j]))continue;
+			int temp = CK(v[i], v[j]);
+			//cout << i << " " << j << " " << temp << "\n";
+			if (temp) {
+				unionParent(v[i], v[j]);
+			}
+		}
+	}
+	
+	/*
+	for (int i = 0; i < N; i++) {
+		cout << i << " : ";
+		cout << set[i] << " : ";
+		cout << SIZE[i] << "\n";
+	}
+	*/
+	
+
+	cout << cnt << "\n" << MAX << "\n";
+
+	return 0;
+}
