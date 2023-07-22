064516fa20c1b086f543bcbdf61ccdcf2bfcdda1
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2023-07-22 22:56:20 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int N;
+    cin >> N;
+    
+    for(int i=0;i<N;i++){
+        int num;
+        cin >> num;
+        cout << num << " " << num << "\n";
+    }
+    
+    return 0;
+}
