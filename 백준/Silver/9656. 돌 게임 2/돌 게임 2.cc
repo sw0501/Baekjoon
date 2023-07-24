b8adb5ab8f33e75ae55490b5223a7903f055b411
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2023-07-24 21:18:43 +0900

    [Silver IV] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int N;
+    
+    cin >> N;
+    
+    if(N%2 == 0){
+        cout << "SK";
+    }
+    else cout << "CY";
+    
+    //1 창영
+    //2 상근
+    //3 창영
+    //4 상근
+    //5 창영
+    //6 상근
+    
+    return 0;
+}
