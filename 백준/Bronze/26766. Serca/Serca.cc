ab9d5014470d218889a7f5b26b515a1839a9f5be
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2023-08-05 15:18:04 +0900

    [Bronze V] Title: Serca, Time: 152 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main(){
+    
+    int N;
+    scanf("%d",&N);
+    
+    
+    for (int i=0;i<N;i++){
+        printf(" @@@   @@@ ");
+        printf("\n");
+        printf("@   @ @   @");
+        printf("\n");
+        printf("@    @    @");
+        printf("\n");
+        printf("@         @");
+        printf("\n");
+        printf(" @       @ ");
+        printf("\n");
+        printf("  @     @  ");
+        printf("\n");
+        printf("   @   @   ");
+        printf("\n");
+        printf("    @ @    ");
+        printf("\n");
+        printf("     @     ");
+        printf("\n");
+    }
+    
+    return 0;
+}
+
+
+ 
+
+
+
+ 
+  
+   
+    
+     
