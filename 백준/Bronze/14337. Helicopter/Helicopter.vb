c85f012fe92e1f1e05babfbcb193d0d3042f1152
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2023-08-10 19:45:31 +0900

    [Bronze V] Title: Helicopter, Time: 60 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/14337. Helicopter/Helicopter.vb
@@ -0,0 +1,16 @@
+Module Main
+ 
+  Sub Main()
+    Dim s() As String = Nothing
+ 
+    Console.WriteLine("      ===================")
+    Console.WriteLine("          ____||___")
+    Console.WriteLine("\ /      /       []\")
+    Console.WriteLine(" X=======           \__")
+    Console.WriteLine("/ \      \____________|")
+    Console.WriteLine("            ||  ||")
+    Console.WriteLine("         \-----------/")
+        
+  End Sub
+ 
+End Module
