e3c37e89f2056a2a248938dac7f2b1ae0da0a534
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2023-09-03 18:03:35 +0900

    [Gold II] Title: 피보나치 수 3, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef vector<vector<long long>> matrix;
+
+matrix multiply(matrix&A, matrix&B) {
+	matrix C = { {0,0},{0,0} };
+
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 2; j++) {
+			for (int k = 0; k < 2; k++) {
+				C[i][j] = (C[i][j] + A[i][k] * B[k][j]) % 1000000;
+			}
+		}
+	}
+	return C;
+}
+
+
+int main() {
+
+	long long n;
+	cin >> n;
+
+	matrix ans = { {1,0},{0,1} };
+	matrix A = { {1,1},{1,0} };
+
+	//F[n] = F[n-1] + F[n-2]
+	//F[n-1] = F[n-2] + F[n-3];
+	//F[n-2] = F[n-3] + F[n-4]
+
+	while (n > 0) {
+		if (n % 2 == 1) {
+			ans = multiply(ans, A);
+		}
+		A = multiply(A, A);
+		n /= 2;
+	}
+	cout << ans[0][1] << "\n";
+
+	
+
+
+	return 0;
+}
