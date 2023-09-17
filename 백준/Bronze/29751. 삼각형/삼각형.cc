ba78525b4b2b76128a470edc43604aeed41227b2
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2023-09-17 18:25:29 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdio.h>
+
+
+using namespace std;
+
+int main(){
+    
+    int W,H;
+    cin >> W >> H;
+    
+    printf("%.1lf",(double)W*H/2);
+    
+    
+    return 0;
+}
