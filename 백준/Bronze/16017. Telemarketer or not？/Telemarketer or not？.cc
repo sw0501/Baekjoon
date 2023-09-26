dea3c1e9c36cdf75c7639675f53cce095d7f916d
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2023-09-26 19:38:44 +0900

    [Bronze IV] Title: Telemarketer or not?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16017. Telemarketer or not？/Telemarketer or not？.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+    
+    if((a==8||a==9) && (b==c) && (d==8||d==9)){
+        cout << "ignore";
+    }
+    else cout << "answer";
+    return 0;
+}
