46db0925d0e0f862ba6c37996d145a2a885bbd85
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2023-10-25 19:56:00 +0900

    [Bronze IV] Title: Laptop Sticker, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21591. Laptop Sticker/Laptop Sticker.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int wc,hc,ws,hs;
+    cin >> wc >> hc>> ws >> hs;
+    
+    if(wc-2>=ws && hc-2>=hs)cout << 1;
+    else cout << 0;
+    
+    
+    return 0;
+}
