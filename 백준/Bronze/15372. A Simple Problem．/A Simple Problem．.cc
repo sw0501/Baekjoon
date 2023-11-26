45ab513f40c5dd9e67c807514d19f89ce5c9066a
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2023-11-26 19:46:40 +0900

    [Bronze IV] Title: A Simple Problem., Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15372. A Simple Problem．/A Simple Problem．.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    int T;
+    cin >> T;
+    
+    for(int i=0;i<T;i++){
+        long long N;
+        cin >> N;
+        cout << N*N << "\n";
+    }
+    
+    
+}
