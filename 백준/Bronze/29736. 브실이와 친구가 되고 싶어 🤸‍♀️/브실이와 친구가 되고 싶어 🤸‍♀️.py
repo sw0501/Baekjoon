694c59f72e98f868f18cf661e8ec1ea98af77804
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2023-12-17 05:55:34 +0900

    [Bronze IV] Title: 브실이와 친구가 되고 싶어 🤸‍♀️, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/29736. 브실이와 친구가 되고 싶어 🤸‍♀️/브실이와 친구가 되고 싶어 🤸‍♀️.py
@@ -0,0 +1,12 @@
+a,b = map(int,input().split())
+k,x = map(int,input().split())
+
+cnt = 0
+
+for i in range(k-x,k+x+1):
+    if i >=a and b>= i:
+        cnt = cnt+1
+if cnt == 0:
+    print("IMPOSSIBLE")
+else:
+    print(cnt)
