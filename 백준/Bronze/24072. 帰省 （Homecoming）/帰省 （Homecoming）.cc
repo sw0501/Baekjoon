464cd1f341f3590fa3cc39c7901b34e52ee3cf66
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-01-16 22:18:44 +0900

    [Bronze IV] Title: 帰省 (Homecoming), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24072. 帰省 （Homecoming）/帰省 （Homecoming）.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    if (C >= A && C < B) {
+        cout << 1;
+    }
+    else cout << 0;
+
+    return 0;
+}
