b280701b944732ac32627448d97f26452f77ea3e
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-01-25 10:13:48 +0900

    [Gold II] Title: 사전, Time: 124 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Gold/1256. 사전/사전.java
+++ 백준/Gold/1256. 사전/사전.java
@@ -15,6 +15,8 @@ public class Main {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		//n,m,k 입력
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
@@ -23,6 +25,7 @@ public class Main {
 		dp = new long[n + 1][m + 1];
 		dp[0][0] = 1;
 
+		//조합 DP로 계산
 		for (int i = 1; i <= n; i++) {
 			dp[i][0] = 1;
 			for (int j = 1; j <= m; j++) {
@@ -33,9 +36,16 @@ public class Main {
 			}
 		}
 
+		//k가 조합의 개수보다 크다면
 		if (dp[n][m] < k) {
 			System.out.println(-1);
 		} else {
+			//맨 앞자리부터 계산 
+			//dp[i][m] -> 앞자리에 a를 둔 상태에서 나머지 조합의 개수
+			//dp[i][m] >= k -> k보다 크다면 
+			//[a ~~~]  <= k번째 문자열 < [z ~~~] 가 되기 때문에 반드시 앞자리에 a가 와야됨
+			//작다면 z 추가 후 앞자리가 a인 조합의 개수만큼 k 감소
+			//z를 추가했으므로 m도 1 감소
 			for (int i = n - 1; i >= 0; i--) {
 				if (dp[i][m] >= k) {
 					sb.append("a");
