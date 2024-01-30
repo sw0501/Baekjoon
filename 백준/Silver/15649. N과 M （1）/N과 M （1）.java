44aa14c66da68281b6cb982cb4eccb3eaf6b20cf
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-01-30 15:11:34 +0900

    [Silver III] Title: N과 M (1), Time: 912 ms, Memory: 44584 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int ck[] = new int[9];
+	static int N;
+	static int M;
+
+	public static void recursive(int level, String str) {
+		for (int i = 1; i <= N; i++) {
+			if (ck[i] == 0) {
+				ck[i] = 1;
+				if (level == M) {
+					System.out.println(str + i + " ");
+				} else {
+					recursive(level + 1, str + i + " ");
+				}
+				ck[i] = 0;
+			}
+		}
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		recursive(1, "");
+	}
+
+}
