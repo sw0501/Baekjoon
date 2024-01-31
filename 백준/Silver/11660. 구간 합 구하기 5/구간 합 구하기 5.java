e5bfda70217aad9cd60de3c37d1ec6a3e3daa077
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-01-31 14:31:38 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 856 ms, Memory: 121012 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		int[][] arr = new int[N + 1][N + 1];
+		int[][] DP = new int[N + 1][N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= N; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				DP[i][j] = DP[i][j - 1] + DP[i - 1][j] - DP[i - 1][j - 1] + arr[i][j];
+			}
+
+		}
+
+		int startX, startY, endX, endY;
+		for (int i = 1; i <= M; i++) {
+			st = new StringTokenizer(br.readLine());
+			startX = Integer.parseInt(st.nextToken());
+			startY = Integer.parseInt(st.nextToken());
+			endX = Integer.parseInt(st.nextToken());
+			endY = Integer.parseInt(st.nextToken());
+
+			sb.append(DP[endX][endY] - DP[startX - 1][endY] - DP[endX][startY - 1] + DP[startX - 1][startY - 1])
+					.append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+}
