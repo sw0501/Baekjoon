d3234ec8c2ddf7070451532a2208e8a2ab792eae
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-02-23 21:16:48 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.py
@@ -0,0 +1,24 @@
+N = int(input())
+
+if N==1:
+    print(str(12) + " " + str(1600))
+elif N==2:
+    print(str(11) + " " + str(894))
+elif N==3:
+    print(str(11) + " " + str(1327))
+elif N==4:
+    print(str(10) + " " + str(1311))
+elif N==5:
+    print(str(9) + " " + str(1004))
+elif N==6:
+    print(str(9) + " " + str(1178))
+elif N==7:
+    print(str(9) + " " + str(1357))
+elif N==8:
+    print(str(8) + " " + str(837))
+elif N==9:
+    print(str(7) + " " + str(1055))
+elif N==10:
+    print(str(6) + " " + str(556))
+elif N==11:
+    print(str(6) + " " + str(773))
