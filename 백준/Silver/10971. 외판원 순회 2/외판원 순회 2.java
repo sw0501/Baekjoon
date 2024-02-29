29918c0a0af8165ea980b1571c7446a9546e657d
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-02-29 13:55:11 +0900

    [Silver II] Title: 외판원 순회 2, Time: 268 ms, Memory: 12196 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,62 +1,56 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.math.*;
+import java.nio.Buffer;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
+	static int			ans			= 987654321;
+    static int          N;
+    static int[][]		prices 		= new int[11][11];
+    static boolean[]	visited 	= new boolean[11];
 
-	static int N;
-	static int dist[][];
-	static boolean visit[];
-	static int minCost = Integer.MAX_VALUE;
-
-	static void select(int start, int city, int cnt, int cost) {
-		
-		visit[city] = true;
-		if (cnt == N-1) {
-			if(dist[city][start] !=0) {
-				minCost = Math.min(minCost, cost + dist[city][start]);	
-			}
-		}
-
-		for (int i = 0; i < N; i++) {
-			if (!visit[i] && dist[city][i] != 0) {
-				select(start, i, cnt + 1, cost + dist[city][i]);
-			}
-		}
-
-		visit[city] = false;
-	}
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-		N = Integer.parseInt(in.readLine());
-		dist = new int[N][N];
-		visit = new boolean[N];
-
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(in.readLine());
-			for (int j = 0; j < N; j++) {
-				dist[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-
-		for (int i = 0; i < N; i++) {
-			select(i, i, 0, 0);
-		}
-
-		System.out.println(minCost);
-	}
+    public static void main(String[] args) throws Exception {
+//        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader      br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder       sb = new StringBuilder();
+        StringTokenizer     st;
+        
+        N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	for (int j = 1; j <= N; j++) {
+        		prices[i][j] = Integer.parseInt(st.nextToken());
+        	}
+        }
+        
+        for (int i = 1; i <= N; i++) {
+        	visited[i] = true;
+        	traverse(i, 1, i, 0);
+        	visited[i] = false;        	
+        }
+        System.out.println(ans);
+    }
+    
+    static void traverse(int cur, int count, int start, int sum) {
+    	if (count == N) {
+    		if (prices[cur][start] != 0) {
+    			sum += prices[cur][start];
+    			ans = Math.min(ans, sum);    			
+    		}
+    		return;
+    	}
+    	for (int i = 1; i <= N; i++) {
+    		if (visited[i]) continue;
+    		if (prices[cur][i] == 0) continue;
+    		visited[i] = true;    		
+    		traverse(i, count + 1, start, sum + prices[cur][i]);
+    		visited[i] = false;
+    	}
+    	
+    }
 }
