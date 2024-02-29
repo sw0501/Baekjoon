5a3da4b5a536c90869622593a294f901e5969791
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-02-29 14:25:38 +0900

    [Gold IV] Title: 연구소, Time: 832 ms, Memory: 296808 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,116 +1,126 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
+class Pair {
+	int first;
+	int second;
+	public Pair(int first, int second) {
+		this.first = first;
+		this.second = second;
+	}
+	
+}
 public class Main {
 
-	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
+    static int          N;
+    static int			M;
+    static int[][]		board 	= new int[9][9];    
+    static int[][]		copy 	= new int[9][9];    
+    static boolean[][]	visited = new boolean[9][9];
+    static final int[]	dx 		= {-1, 0, 1, 0};
+    static final int[]	dy 		= {0, 1, 0, -1};
+    static int			ans 	= 0;
 
-	static int N, M;
-	static int map[][];
-	static int ans = 0;
-
-	static class Pair {
-		int x, y;
-
-		public Pair(int x, int y) {
-			super();
-			this.x = x;
-			this.y = y;
-		}
-
-	}
-
-	static void select(int cnt) {
-		if (cnt == 3) {
-			boolean visit[][] = new boolean[N][M];
-
-			Queue<Pair> queue = new LinkedList<>();
-
-			int dx[] = { 1, -1, 0, 0 };
-			int dy[] = { 0, 0, 1, -1 };
-
-			int temp[][] = new int[N][M];
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
-					temp[i][j] = map[i][j];
-					if (temp[i][j] == 2) {
-						visit[i][j] = true;
-						queue.add(new Pair(i, j));
-					}
-				}
-			}
-
-			while (!queue.isEmpty()) {
-				Pair p = queue.poll();
-
-				for (int k = 0; k < 4; k++) {
-					int tx = p.x + dx[k];
-					int ty = p.y + dy[k];
-
-					if (tx >= 0 && tx < N && ty >= 0 && ty < M) {
-						if (!visit[tx][ty] && temp[tx][ty] == 0) {
-							visit[tx][ty] = true;
-							temp[tx][ty] = 2;
-							queue.add(new Pair(tx, ty));
-						}
-					}
-				}
-			}
-
-			int tempCnt = 0;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
-					if (temp[i][j] == 0)
-						tempCnt++;
-				}
-			}
-
-			ans = Math.max(ans, tempCnt);
-
-			return;
-		}
-
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (map[i][j] == 0) {
-					map[i][j] = 1;
-					select(cnt + 1);
-					map[i][j] = 0;
-				}
-			}
-		}
-	}
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-
-		st = new StringTokenizer(in.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-
-		map = new int[N][M];
-		
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(in.readLine());
-			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-
-		select(0);
-
-		System.out.println(ans);
-	}
+    public static void main(String[] args) throws Exception {
+//        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader      br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder       sb = new StringBuilder();
+        StringTokenizer     st;
+        
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        for (int i = 1; i <= N; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	for (int j = 1; j <= M; j++) {
+        		board[i][j] = Integer.parseInt(st.nextToken());
+        		copy[i][j] = board[i][j];
+        	}
+        }
+        
+        for (int x1 = 1; x1 <= N; x1++) {
+        	for (int y1 = 1; y1 <= M; y1++) {
+        		for (int x2 = 1; x2 <= N; x2++) {
+                	for (int y2 = 1; y2 <= M; y2++) {                		
+                		if (x1 == x2 && y1 == y2) continue;
+                		
+                		for (int x3 = 1; x3 <= N; x3++) {
+                        	for (int y3 = 1; y3 <= M; y3++) {
+                        		if (x1 == x3 && y1 == y3) continue;
+                        		if (x2 == x3 && y2 == y3) continue;
+                        		if (board[x1][y1] != 0 || board[x2][y2] != 0 || board[x3][y3] != 0) continue;                        		
+                        		board[x1][y1] = 1;
+                        		board[x2][y2] = 1;
+                        		board[x3][y3] = 1;                        		
+                        		ans = Math.max(ans, bfs());                 
+                        		init();
+                        	}
+                        }
+                	}
+                }
+        	}
+        }
+        System.out.println(ans);
+        
+    }
+    static void init() {
+    	for (int i = 1; i <= N; i++) {
+    		for (int j = 1; j <= M; j++) {
+    			board[i][j] = copy[i][j];
+    			visited[i][j] = false;
+    		}
+    	}
+    }
+    static void printBoard() {
+    	for (int i = 1; i <= N; i++) {
+    		for (int j = 1; j <= M; j++) {
+    			System.out.print(board[i][j] + " ");
+    		}
+    		System.out.println();
+    	}
+    	System.out.println();
+    }
+    
+    static int bfs() {
+    	int ret = 0;    	
+    	Queue<Pair> q = new ArrayDeque<>();    	
+    	
+    	for (int i = 1; i <= N; i++) {
+    		for (int j = 1; j <= M; j++) {
+    			if (board[i][j] == 2) {
+    				q.add(new Pair(i, j));
+    				visited[i][j] = true;
+    			}
+    		}
+    	}
+    	
+    	while (!q.isEmpty()) {
+    		int x = q.peek().first;
+    		int y = q.peek().second;
+    		q.poll();
+    		for (int k = 0; k < 4; k++) {
+    			int nx = x + dx[k];
+    			int ny = y + dy[k];
+    			if (nx < 1 || ny < 1 || nx > N || ny > M) continue;
+    			if (visited[nx][ny]) continue;
+    			if (board[nx][ny] != 0) continue;
+    			visited[nx][ny] = true;
+    			board[nx][ny] = 2;
+    			q.add(new Pair(nx, ny));
+    		}
+    	}
+    	
+    	for (int i = 1; i <= N; i++) {
+    		for (int j = 1; j <= M; j++) {
+    			if (board[i][j] == 0) ret++;
+    		}
+    	}
+    	
+        	
+    	return ret;    	
+    }
 }
