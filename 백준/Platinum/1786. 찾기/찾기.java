5038e2ed7b1ad7cfcbcb7450cd1af2d6dc0cf0ea
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-03-31 02:46:07 +0900

    [Platinum V] Title: 찾기, Time: 472 ms, Memory: 64568 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.java
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -1,13 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
@@ -15,22 +7,19 @@ public class Main {
 	static StringBuilder sb = new StringBuilder();
 	static StringTokenizer st = null;
 
-	static int N;
-
-	//접두사 접미사
-	static int[] getPI(String p) {
+	static int[] getPi(String p) {
 		int m = p.length(), j = 0;
 
 		int[] pi = new int[m];
 
 		for (int i = 1; i < m; i++) {
-			//돌아갈 위치가 있고 서로 다른 경우
+			// 일치하는 접미사를 찾을 때까지 반복
 			while (j > 0 && p.charAt(i) != p.charAt(j)) {
 				j = pi[j - 1];
 			}
-			//접두사와 접미사가 일치하는 경우
+
+			// 일치한다면 돌아갈 지점 선택
 			if (p.charAt(i) == p.charAt(j)) {
-				//실패시 돌아갈 위치 지정
 				pi[i] = ++j;
 			}
 		}
@@ -38,53 +27,41 @@ public class Main {
 		return pi;
 	}
 
-	//KMP 함수
-	static List<Integer> KMP(String s, String p) {
-		//정답 리스트
-		List<Integer> ans = new ArrayList<>();
-		//접두사 접미사 일치 개수
-		int[] pi = getPI(p);
+	public static void main(String[] args) throws IOException {
+		String T, P;
 
-		int n = s.length(), m = p.length(), j = 0;
+		T = br.readLine();
+		P = br.readLine();
+
+		List<Integer> list = KMP(T, P, getPi(P));
+		
+		System.out.println(list.size());
+		for(int i:list) {
+            sb.append(i+1).append(" ");
+		}
+        System.out.println(sb);
+	}
+
+	private static List<Integer> KMP(String t, String p, int[] pi) {
+		// TODO Auto-generated method stub
+List<Integer> list = new ArrayList<>();
+		int n = t.length(), m = p.length(), j = 0;
 
 		for (int i = 0; i < n; i++) {
-			//일치하지 않는 경우
-			while (j > 0 && s.charAt(i) != p.charAt(j)) {
-				//실패 위치로 돌아
+			while (j > 0 && t.charAt(i) != p.charAt(j)) {
 				j = pi[j - 1];
 			}
-			//일치하는 경우
-			if (s.charAt(i) == p.charAt(j)) {
-				//마지막까지 전부 일치하는 경우
+
+			if (t.charAt(i) == p.charAt(j)) {
 				if (j == m - 1) {
-					//ans에 추가
-					ans.add(i - m + 1);
-					//돌아갈 위치
+					list.add(i - m + 1);
 					j = pi[j];
 				} else {
-					//j 개수 증가
 					j++;
 				}
 			}
 		}
-		return ans;
+
+		return list;
 	}
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-
-		//문자열 입력
-		String s = br.readLine();
-		String p = br.readLine();
-
-		//KMP 실행
-		List<Integer> ans = KMP(s, p);
-
-		System.out.println(ans.size());
-		for (int i : ans) {
-			System.out.print(i + 1 + " ");
-		}
-
-	}
-
 }
