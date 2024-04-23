02e48a04e635e4df3a9fd6dad57b17d2580393c7
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-04-24 01:16:22 +0900

    [Bronze IV] Title: 특별한 가지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int N,M,K;
+    cin >> N >> M >> K;
+    
+    int a = M/N;
+    int b = a*K;
+        
+        cout << b << "\n";
+    
+    
+    return 0;
+}
