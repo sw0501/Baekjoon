2a1bff017025e94abb87b3f246b36d181e71653b
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-08-26 21:04:59 +0900

    [Bronze IV] Title: Acres, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26532. Acres/Acres.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    
+    int a,b;
+    cin >> a >> b;
+    
+    cout << ceil((double)(a * b) / 4840 / 5) << "\n";
+    
+    
+    
+    return 0;
+}
