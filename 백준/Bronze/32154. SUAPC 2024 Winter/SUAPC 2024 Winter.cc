6ee02a0eef6423e1e726b4af938e4696d3379495
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-09-03 00:38:53 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.cc
@@ -0,0 +1,52 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int N;
+    cin >> N;
+    
+    if(N==1){
+        cout << 11 << "\n";
+        cout << "A B C D E F G H J L M\n";
+    }
+    else if(N==2){
+        cout << 9 << "\n";
+        cout << "A C E F G H I L M\n";
+    }
+    else if(N==3){
+        cout << 9 << "\n";
+        cout << "A C E F G H I L M\n";
+    }
+    else if(N==4){
+        cout << 9 << "\n";
+        cout << "A B C E F G H L M\n";
+    }
+    else if(N==5){
+        cout << 8 << "\n";
+        cout << "A C E F G H L M\n";
+    }
+    else if(N==6){
+        cout << 8 << "\n";
+        cout << "A C E F G H L M\n";
+    }
+    else if(N==7){
+        cout << 8 << "\n";
+        cout << "A C E F G H L M\n";
+    }
+    else if(N==8){
+        cout << 8 << "\n";
+        cout << "A C E F G H L M\n";
+    }
+    else if(N==9){
+        cout << 8 << "\n";
+        cout << "A C E F G H L M\n";
+    }
+    else if(N==10){
+        cout << 8 << "\n";
+        cout << "A B C F G H L M\n";
+    }
+    
+    return 0;
+}
