868af8321d3cc3046ad8772d8d8ce2a3703485f2
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-09-20 02:09:32 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    // 코드를 작성해주세요
+    int a,b,c;
+    
+    cin >> a >> b >> c;
+    
+    cout << a*3 + b*4 + c*5 << "\n";
+    return 0;
+}
