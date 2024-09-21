90bff67f58792f2add94560ba8952743d2d64c35
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-09-22 02:53:01 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+
+    int N;
+    cin >> N;
+    
+    if(N%2==0){
+        cout << "SciComLove" <<"\n";
+    }
+    else{
+        cout << "evoLmoCicS" <<"\n";
+    }
+    
+    return 0;
+}
