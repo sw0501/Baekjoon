509c078268c102c4831f61de7da51cd95decbe4e
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-09-25 00:31:34 +0900

    [Bronze IV] Title: 코드마스터 2024, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32215. 코드마스터 2024/코드마스터 2024.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    // 코드를 작성해주세요
+    int n,m,k;
+    cin >> n >> m >> k;
+    
+    int ans = k*m + m;
+    
+    cout << ans;
+    return 0;
+}
