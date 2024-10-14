56045c249c17ad02849c23f6114034c97f1e2211
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-10-15 00:51:18 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -1,50 +1,31 @@
-//들어갈 자료의 크기보고 적당한 자료형을 쓰자 ex) 매우매우 큰 정수형 -> long long
-#include<iostream>
-#include<algorithm>
+#include <iostream>
 
 using namespace std;
 
-int N;		//테스트 케이스
-int M;		//구할 피보나치 수열
-int f1=0;	//1번 수
-int f2=0;	//2번 수
-int num=0;	//피보나치 수열
 
-void init();
-void Fibo(int n);
+int DP[41][2] = {0,};
 
-int main(){
-	
-	init();
-	
-	return 0;
-}
+int main() 
+{
+    // 코드를 작성해주세요
+    int T;
+    cin >> T;
 
-void init(){
-	cin >> N;
-	for(int i=0;i<N;i++){
-		cin >> M;
-		Fibo(0);
-	}
-}
-
-void Fibo(int n){
-	if(n==0){
-		f1=1;
-		f2=0;
-	}
-	else if(n==1){
-		f1=0;
-		f2=1;
-	}
-	else {
-		int temp = f1+f2;
-		f1 = f2;
-		f2 = temp;
-	}
-	if(n==M){
-		cout<<f1<<" "<<f2<<"\n";
-		return;
-	}
-	Fibo(n+1);
+    DP[0][0] = 1;
+    DP[0][1] = 0;
+    DP[1][0] = 0;
+    DP[1][1] = 1;
+    for(int i=2;i<41;i++){
+        DP[i][0] = DP[i-1][0] + DP[i-2][0];
+        DP[i][1] = DP[i-1][1] + DP[i-2][1];
+    }
+    
+    for(int i=0;i<T;i++){
+         int num;
+        cin >> num;
+        
+        cout << DP[num][0] << " " << DP[num][1] << "\n";
+    }
+    
+    return 0;
 }
