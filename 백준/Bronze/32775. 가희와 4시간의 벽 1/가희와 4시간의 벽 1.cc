08f19ea48eda35d535bb9432e1197a66a6a25b0c
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-12-01 20:04:49 +0900

    [Bronze V] Title: 가희와 4시간의 벽 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    // 코드를 작성해주세요
+    
+    int s,f;
+    cin >> s >> f;
+    
+    if(s<=f){
+        cout << "high speed rail";
+    }
+    else{
+        cout << "flight";
+    }
+    
+    return 0;
+}
