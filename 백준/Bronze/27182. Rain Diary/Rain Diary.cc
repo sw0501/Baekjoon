d1fcbadbc86d40529e8995160cfeeebaafe62cc7
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-12-24 16:14:54 +0900

    [Bronze IV] Title: Rain Diary, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27182. Rain Diary/Rain Diary.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    // 코드를 작성해주세요
+    
+    int n,m;
+    cin >> n >> m;
+    
+    if(n >= 8){
+        cout << n-7;
+    }
+    else{
+        cout << m + 7;
+    }
+    
+    return 0;
+}
